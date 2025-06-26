85bd06d95ca19c5cc259857a365d7ad1b0a95ea4
minimin0e <gimmiegimmie0102@gmail.com>
2025-06-26 16:37:50 +0700

    First Commit

---
+++ 01 lec/Testprint.py
@@ -0,0 +1,9 @@
+# print("hello wprld")
+# print("Panatchaya")
+# print("i'm hungry")
+# print("I am good \"I will do my best\"")
+# print('/\\')
+print("\ta\na\ta\ta\n\ta")
+print('''\ta
+a\ta\ta
+\ta''')
