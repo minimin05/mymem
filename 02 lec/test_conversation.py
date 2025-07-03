7a91b931446ac4b3c1b34cd13434790c26e471f2
minimin0e <gimmiegimmie0102@gmail.com>
2025-07-03 16:01:20 +0700

    end lc2

---
+++ 02 lec/test_conversation.py
@@ -0,0 +1,26 @@
+age = 25
+height = float (25)
+print(height)
+
+height = 5.6
+age = int(5.6)
+print(age)
+
+num_str = "123"
+num = int(123)
+print(num)
+
+x = 15
+y = 4
+print('x+y=', x+y)
+print('x-y=', x - y)
+print('x*y=', x * y)
+print('x/y=', x / y)
+print('x//y=', x // y)
+print('x%y=', x % y)
+print('x**y=', x ** y)
+
+print('x==y=', x == y)
+print('x!=y=', x != y) 
+print('x>y=', x > y)
+print('x<y=', x < y)
---
+++ 02 lec/test_convert.py
@@ -0,0 +1,13 @@
+# number = input("Enter a number: ")
+# RESULT = int(number) + 10
+# print(RESULT)
+
+name = input("What is your name: ")
+age = int(input("What is your age: "))
+income = float(input("What is your income: "))
+
+print('Here is the data you entered:')
+print('Name:', name)
+print('Age:', age)
+print('Income: ', format(income, '12,.2f'))
+
