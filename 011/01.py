789de47528f5c3b0dddf4641d2655806ecf93363
minimin0e <gimmiegimmie0102@gmail.com>
2025-07-17 20:01:50 +0700

    end011

---
+++ 011/01.py
@@ -0,0 +1,37 @@
+# name = "KMUTNB"
+
+# if name == "KMUTNB":
+#     print("Hello")
+# elif name == "KMITL":
+#     print("Goodbye")
+# else:
+#     print("unknow")
+
+# name = input("Enter your name: ")
+# print("Hello",name)
+
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print(f"{num} is even" )
+# else:
+#     print(f"{num} is odd" )
+
+# score1 = input("Enter score 1: ")
+# score2 = input("Enter score 2: ")
+# score3 = input("Enter score 3: ") 
+
+# average = (int(score1) + int(score2) + int(score3)) / 3
+# if average >= 60:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+# age = int(input("Enter your age: "))
+# if age < 13:
+#     print("You are a child.")
+# elif age <= 19:
+#     print("You are a teenager.")
+# elif age <= 59:
+#     print("You are an adult.")
+# else:
+#     print("You are a senior citizen.")
